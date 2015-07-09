270b51207293f416115cc06e12546df0d9b906ae
sestrimski <sestrimski@gmail.com>
2015-07-09 18:36:44 +0300

    Lecture 2 - presentations and examples

---
+++ examples/02-java-basics/java-basics/src/ObjectType.java
@@ -0,0 +1,16 @@
+public class ObjectType {
+	
+    public static void main(String[] args) {
+        Object myObject = 9;
+        System.out.print("The value of myObject is: ");
+        System.out.println(myObject);
+
+        myObject = "Apple";
+        System.out.print("The value of myObject is: ");
+        System.out.println(myObject);
+        
+        myObject = null;
+        System.out.print("The value of myObject is: ");
+        System.out.println(myObject);
+    }
+}
