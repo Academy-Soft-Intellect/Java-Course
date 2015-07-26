6df286b8d74e74c961b8b698eea8b1e48a73b6aa
sestrimski <sestrimski@gmail.com>
2015-07-26 17:27:29 +0300

    Lecture 6 and samples

---
+++ examples/06-classes-and-objects/ClassesAndObjects/src/ClassesBasic.java
@@ -0,0 +1,12 @@
+public class ClassesBasic {
+
+	public static void main(String[] args) {
+		
+		Animal rex = new Animal("Rex", 5);
+		rex.bark();
+		
+		Animal sharo = new Animal("Sharo", 2);
+		sharo.bark();
+	}
+
+}
