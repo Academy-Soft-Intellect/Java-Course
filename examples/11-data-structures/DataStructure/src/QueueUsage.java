ffc721caa1eae4f2b5d387f6b0fcf462fe9e6921
sestrimski <sestrimski@gmail.com>
2015-08-11 18:34:43 +0300

    Lecture 11 and samples

---
+++ examples/11-data-structures/DataStructure/src/QueueUsage.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;;
+
+public class QueueUsage {
+
+	public static void main(String[] args) {
+		
+		Queue<String> queue = new LinkedList<String>();
+		
+		queue.offer("One");
+		
+		queue.offer("Two");
+		
+		queue.offer("Three");
+		
+		queue.offer("Four");
+		
+		while (!queue.isEmpty()) {
+			
+			String current = queue.poll();
+			
+		    System.out.println(current);
+		    
+		}
+	}
+}
+
