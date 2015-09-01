da33c6d0873e95473dd052e6dabb12805a9e1c84
sestrimski <sestrimski@gmail.com>
2015-09-01 18:52:38 +0300

    Added Lecture 14 plus samples and remove some test code

---
+++ examples/14-unit-tests/.metadata/.plugins/org.eclipse.core.resources/.history/2b/c0283d7a375000151891bb95adc64747
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ examples/14-unit-tests/UnitTest/src/Academy.java
@@ -0,0 +1,16 @@
+public class Academy {
+	
+	public boolean IsJavaPowerful(){
+		return true;
+		
+	}
+	
+	public int multiply(int a, int b){
+		return a * b;
+		
+	}
+	
+
+}
+
+
