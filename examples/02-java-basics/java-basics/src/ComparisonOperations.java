2c74399227bccb21c04fa402c4e2b691cf0e3b65
sestrimski <sestrimski@gmail.com>
2015-11-21 11:51:11 +0200

    Lecture 2 and examples

---
+++ examples/02-java-basics/java-basics/src/ComparisonOperations.java
@@ -0,0 +1,38 @@
+public class ComparisonOperations {
+	
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4;
+
+        System.out.print(" a = ");
+        System.out.println(a);
+        
+        System.out.print(" b = ");
+        System.out.println(b);
+        
+        boolean aEqualsB = a == b;
+        System.out.print(" a == b : ");
+        System.out.println(aEqualsB);
+
+        boolean aDiffersB = a != b;
+        System.out.print(" a != b : ");
+        System.out.println(aDiffersB);
+
+        boolean aIsLessThanB = a < b;
+        System.out.print(" a < b : ");
+        System.out.println(aIsLessThanB);
+
+        boolean aIsGreaterThanB = a > b;
+        System.out.print(" a > b : ");
+        System.out.println(aIsGreaterThanB);
+
+        boolean aIsLessOrEqualThanB = a <= b;
+        System.out.print(" a <= b : ");
+        System.out.println(aIsLessOrEqualThanB);
+
+        boolean aIsGreaterOrEqualThanB = a >= b;
+        System.out.print(" a >= b : ");
+        System.out.println(aIsGreaterOrEqualThanB);
+    }
+}
