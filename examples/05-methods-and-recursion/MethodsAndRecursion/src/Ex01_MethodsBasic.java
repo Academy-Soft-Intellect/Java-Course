af0864bc9b1ecc6a8044880bd3907a589c29679b
sestrimski <sestrimski@gmail.com>
2015-12-02 18:35:45 +0200

    Lecture 5 and Examples

---
+++ examples/05-methods-and-recursion/MethodsAndRecursion/src/Ex01_MethodsBasic.java
@@ -0,0 +1,19 @@
+public class Ex01_MethodsBasic {
+
+	public static void main(String[] args) {
+		sayHello();
+		
+		System.out.println("I am printed from main");
+		
+		sayGoodBye();
+	}
+	
+	public static void sayHello(){
+		System.out.println("Hello");
+	}
+	
+	public static void sayGoodBye(){
+		System.out.println("Good Bye");
+	}
+
+}
